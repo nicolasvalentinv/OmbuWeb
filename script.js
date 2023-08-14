c363b430631bff5c151b8e3f83ad90e9d6804477
nicolasvalentinv <108905876+nicolasvalentinv@users.noreply.github.com>
2023-08-14 15:59:55 -0300

    new changes

--- script.js
+++ script.js
@@ -1,75 +1,119 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     const niveles = [
         {
-            objetivo: "console.log('Hola mundo');",
-            mensaje: "Usa console.log() para imprimir el mensaje '¡Hola, mundo!' en la consola.",
+            categoria: "Matemáticas",
+            pregunta: "¿Cuánto es 2 + 2?",
+            respuesta: "console.log(4);"
         },
         {
-            objetivo: "console.log('Otro nivel');",
-            mensaje: "Imprime el mensaje 'Otro nivel' usando console.log().",
+            categoria: "Animales",
+            pregunta: "¿Qué animal hace 'mu'?",
+            respuesta: "console.log(Vaca);"
         },
         {
-            objetivo: "console.log('Nuevo objetivo');",
-            mensaje: "Imprime el mensaje 'Nuevo objetivo' usando console.log().",
+            categoria: "Colores",
+            pregunta: "¿Cuál es el color del cielo en un día despejado?",
+            respuesta: "console.log(celeste);"
         },
-        // Puedes agregar más niveles aquí con sus respectivos objetivos y mensajes.
+        {
+            categoria: "Frutas",
+            pregunta: "¿Qué fruta es amarilla por fuera y blanca por dentro?",
+            respuesta: "Banana"
+        },
+        {
+            categoria: "Naturaleza",
+            pregunta: "¿Qué cae del cielo cuando llueve?",
+            respuesta: "Agua"
+        },
+        {
+            categoria: "Formas",
+            pregunta: "¿Cuál es la figura con tres lados?",
+            respuesta: "Triángulo"
+        },
+        {
+            categoria: "Cuerpo humano",
+            pregunta: "¿Dónde están tus orejas?",
+            respuesta: "Cabeza"
+        },
+        {
+            categoria: "Medios de transporte",
+            pregunta: "¿Qué usas para ir a la escuela?",
+            respuesta: "Autobús"
+        },
+        // Agregar más niveles aquí...
     ];
 
     let nivelActual = 0;
     const nivelNumero = document.getElementById("nivelNumero");
-    const nivelTitulo = document.getElementById("nivelTitulo");
-    const objetivo = document.getElementById("objetivo");
-    const codigoInput = document.getElementById("codigoInput");
+    const nivelCategoria = document.getElementById("nivelCategoria");
+    const imagenCategoria = document.getElementById("imagenCategoria");
+    const pregunta = document.getElementById("pregunta");
+    const respuestaInput = document.getElementById("respuestaInput");
     const verificarButton = document.getElementById("verificar");
-    const siguienteButton = document.getElementById("siguiente");
     const siguienteNivelButton = document.getElementById("siguienteNivelBtn");
     const resultado = document.getElementById("resultado");
+    const barraProgreso = document.getElementById("barraProgreso");
+    const codigoInput = document.getElementById("codigoInput");
+
+    const porcentajeIncremento = 100 / niveles.length;
 
     actualizarNivel();
 
     verificarButton.addEventListener("click", function() {
-        const codigoIngresado = codigoInput.value.trim();
-        const objetivoActual = niveles[nivelActual].objetivo;
+        const respuestaIngresada = respuestaInput.value.trim();
+        const respuestaCorrecta = niveles[nivelActual].respuesta;
 
-        if (codigoIngresado === objetivoActual) {
-            resultado.textContent = "¡Correcto! Puedes avanzar al siguiente nivel.";
-            siguienteButton.disabled = false;
+        if (respuestaIngresada === respuestaCorrecta) {
+            resultado.textContent = "¡Respuesta correcta! Puedes avanzar al siguiente nivel.";
             siguienteNivelButton.disabled = false;
+            actualizarBarraProgreso();
         } else {
-            resultado.textContent = "Incorrecto. Inténtalo de nuevo.";
-            siguienteButton.disabled = true;
+            resultado.textContent = "Respuesta incorrecta. Inténtalo de nuevo.";
             siguienteNivelButton.disabled = true;
         }
-    });
-
-    siguienteButton.addEventListener("click", function() {
-        nivelActual++;
-        actualizarNivel();
-        resultado.textContent = "";
-        codigoInput.value = "";
     });
 
     siguienteNivelButton.addEventListener("click", function() {
         nivelActual++;
         actualizarNivel();
         resultado.textContent = "";
-        codigoInput.value = "";
+        respuestaInput.value = "";
+        siguienteNivelButton.disabled = true;
+        actualizarBarraProgreso();
     });
 
     function actualizarNivel() {
         if (nivelActual < niveles.length) {
             nivelNumero.textContent = nivelActual + 1;
-            nivelTitulo.textContent = niveles[nivelActual].titulo;
-            objetivo.textContent = niveles[nivelActual].mensaje;
-            codigoInput.value = "";
-            siguienteButton.disabled = true;
+            nivelCategoria.textContent = niveles[nivelActual].categoria;
+            imagenCategoria.src = niveles[nivelActual].imagen;
+            pregunta.textContent = niveles[nivelActual].pregunta;
+            respuestaInput.value = "";
             siguienteNivelButton.disabled = true;
+            actualizarBarraProgreso();
         } else {
             resultado.textContent = "¡Has completado todos los niveles!";
-            codigoInput.disabled = true;
+            respuestaInput.disabled = true;
             verificarButton.disabled = true;
-            siguienteButton.disabled = true;
             siguienteNivelButton.disabled = true;
         }
     }
+
+    function actualizarBarraProgreso() {
+        const progreso = porcentajeIncremento * nivelActual;
+        barraProgreso.style.width = progreso + "%";
+        barraProgreso.setAttribute("aria-valuenow", progreso);
+        barraProgreso.textContent = `${progreso}%`;
+    
+        if (progreso >= 100) {
+            barraProgreso.style.backgroundColor = "green";
+        } else {
+            barraProgreso.style.backgroundColor = "#007bff"; // Color azul por defecto
+        }
+    }
+
+    
 });
+
+
